c84f2e338c8e31dd7515d97a17964d71ad1271ad
Ashik-Islam <ashik130317010gmail.com>
2025-02-25 18:09:06 +0600

    Initial commit

--- js/custom.js
+++ js/custom.js
@@ -1,42 +1,14 @@
-$('.best_sale_slider').owlCarousel({
-    loop:true,
-    margin:20,
-    nav:true,
-    items:4,
-    autoplay:true,
-    autoplayTimeout:3000,
-    mouseDrag:false
-
-    
-});
-
-$('.diff_brand').owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    mouseDrag:false,
-    dots:true,
-    dotsEach:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        600:{
-            items:3,
-            nav:false
-        },
-        1000:{
-            items:5,
-            nav:false,
-        }
-    }
+$(document).ready(function(){
+    $(".cat_area").click(function(){
+        $(".mega_menu").slideToggle();
+    })
 });
 
 $(document).ready(function(){
-    $("#scroll").click(function(){
-      $("html, body").animate({ scrollTop: 0 }, "slow");
-      return false;
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        autoplay:true,
+        loop:true,
+        dots:true
     });
   });
