e366843c41acd676a08376ce5f35ca4552a14148
Ashik-Islam <ashik130317010gmail.com>
2025-02-28 16:40:47 +0600

    ashik

--- js/custom.js
+++ js/custom.js
@@ -9,17 +9,44 @@ $(document).ready(function(){
         items:1,
         autoplay:true,
         loop:true,
-        dots:true
+        dots:true,
     });
   });
 
-//   search by cat
+//   search by cat slider
 $(document).ready(function(){
     $(".search_cat_slider").owlCarousel({
         items:6,
         autoplay:true,
         loop:true,
-        nav:true
+        nav:true,
+        margin:5,
+        rtl:true
     });
   });
-//   search by cat
+//   search by cat slider
+
+//   deal of the week slider
+$(document).ready(function(){
+    $(".deal_cards").owlCarousel({
+        items:5,
+        autoplay:true,
+        loop:true,
+        nav:true,
+    });
+  });
+//   deal of the week slider
+
+//   popular brands slider
+$(document).ready(function(){
+    $(".popular_brands_slider").owlCarousel({
+        items:6,
+        autoplay:true,
+        loop:true,
+        nav:true,
+        margin:10,
+        rtl:true
+
+    });
+  });
+//   popular brands slider
